b95e5350e44aa0f9c2221ada8372341015c88dda
Isak Kallenbach <kallenbach13@gmail.com>
2020-02-13 18:05:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,7 @@ def gross_for_director(director_data)
   total = 0
   movie_index = 0
   #binding.pry
-  while director_data[:movies][movie_index] < director_data[:movies.to_i].count do
+  while director_data[:movies][movie_index] < director_data[:movies].length do
     total += director_data[:movies][movie_index][:worldwide_gross]
     movie_index += 1
   end
